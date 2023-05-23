e335122c3448661b38932a9ee1caf6c728b7e940
Luis Chang <luis.chang@outlook.com>
2023-05-23 08:25:48 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <!-- <HelloWorld msg="Hello Vue in CodeSandbox!"/> -->
+    <CounterItem />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+import CounterItem from "./components/CounterItem";
+
+export default {
+  name: "App",
+  components: {
+    CounterItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CounterItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h5>{{ fullName }}</h5>
+    <div>
+      <h4>El número actual es ....</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "CounterItem",
+  data() {
+    return {
+      count: 0,
+      fullName: "Luis Chang",
+    };
+  },
+};
+</script>
